f01b538157f67cd959c17806942bd7c75cef7d4e
mayaraza <127501187+mayaraza@users.noreply.github.com>
2024-12-28 15:44:16 +0000

    Add files via upload

---
+++ stimuli.js
@@ -0,0 +1,101 @@
+
+
+// Word stimuli for encoding and recognition tasks
+var master_word_stim = [
+    
+{word: 'ANCHOR', list: 'A'},
+{word: 'ATHLETE', list: 'A'},
+{word: 'BISCUIT', list: 'A'},
+{word: 'BUBBLE', list: 'A'},
+{word: 'DOLPHIN', list: 'A'},
+{word: 'ENZYME', list: 'A'},
+{word: 'GERM', list: 'A'},
+{word: 'GRAVE', list: 'A'},
+{word: 'LUNAR', list: 'A'},
+{word: 'MOLE', list: 'A'},
+{word: 'MOUSTACHE', list: 'A'},
+{word: 'PANTHER', list: 'A'},
+{word: 'SKULL', list: 'A'},
+{word: 'SLEEVE', list: 'A'},
+{word: 'SNORKEL', list: 'A'},
+{word: 'SPACE', list: 'A'},
+
+
+{word: 'ANKLE', list: 'B'},
+{word: 'ARTERY', list: 'B'},
+{word: 'BLADDER', list: 'B'},
+{word: 'CALF', list: 'B'},
+{word: 'CIRCUIT', list: 'B'},
+{word: 'FEAST', list: 'B'},
+{word: 'GLACIER', list: 'B'},
+{word: 'HARDWARE', list: 'B'},
+{word: 'LATTICE', list: 'B'},
+{word: 'OLIVE', list: 'B'},
+{word: 'PARCEL', list: 'B'},
+{word: 'SILK', list: 'B'},
+{word: 'SLEDGE', list: 'B'},
+{word: 'CANVAS', list: 'B'},
+{word: 'STALLION', list: 'B'},
+{word: 'THIGH', list: 'B'},
+
+{word: 'ACROBAT', list: 'C'},
+{word: 'AGENDA', list: 'C'},
+{word: 'CONCRETE', list: 'C'},
+{word: 'FLUTE', list: 'C'},
+{word: 'FROST', list: 'C'},
+{word: 'HOLLOW', list: 'C'},
+{word: 'HORMONE', list: 'C'},
+{word: 'MAGICIAN', list: 'C'},
+{word: 'OYSTER', list: 'C'},
+{word: 'POSTER', list: 'C'},
+{word: 'PRAWN', list: 'C'},
+{word: 'RODENT', list: 'C'},
+{word: 'ROOSTER', list: 'C'},
+{word: 'SAUCER', list: 'C'},
+{word: 'SWORD', list: 'C'},
+{word: 'TONGUE', list: 'C'},
+
+{word: 'CAVE', list: 'D'},
+{word: 'DUCK', list: 'D'},
+{word: 'DWARF', list: 'D'},
+{word: 'FILTER', list: 'D'},
+{word: 'JEWEL', list: 'D'},
+{word: 'KITTEN', list: 'D'},
+{word: 'MOLECULE', list: 'D'},
+{word: 'MONKEY', list: 'D'},
+{word: 'MUSHROOM', list: 'D'},
+{word: 'PANDA', list: 'D'},
+{word: 'PODIUM', list: 'D'},
+{word: 'POPE', list: 'D'},
+{word: 'SOCKET', list: 'D'},
+{word: 'SPATULA', list: 'D'},
+{word: 'TATTOO', list: 'D'},
+{word: 'WOMB', list: 'D'},
+
+];
+
+
+
+
+// Sound stimuli for Relaxation task
+var TFD_stim_arr = [
+    {
+        task: 1,
+    
+        sound_file: './stimuli/relaxation_stim/brown_piano_1.mp3',
+    
+        piano_times: [ 10000, 46000, 112000, 124000, 189000, 260000, 271000 ],
+    
+        colour_times: [ 5000, 17000, 210000, 289000, 320000, 320000 ]
+    },
+    {
+        task: 2,
+    
+        sound_file: './stimuli/relaxation_stim/brown_piano_2.mp3',
+    
+        piano_times: [ 38000, 152000, 226000, 261000, 276000 ],
+    
+        colour_times: [ 18000, 58000, 93000, 182000, 253000, 320000 ],
+    }
+    ];
+
