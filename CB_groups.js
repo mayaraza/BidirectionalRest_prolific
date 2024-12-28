6d7b479816e097f6d51c105dd1fd03c0f48ad778
mayaraza <127501187+mayaraza@users.noreply.github.com>
2024-12-28 11:54:39 +0000

    Add files via upload

---
+++ CB_groups.js
@@ -0,0 +1,16 @@
+
+// Determines pre- / post-encoding tasks
+var experimental_grps = {
+    1 : "RR",   // rest rest
+    2 : "RE",   // rest effort
+    3 : "ER",   // effort rest
+    4 : "EE"    // effort effort
+};
+
+// Determines word list
+var counterbalance_grps = {
+    1 : "",
+    2 : "",
+    3 : "",
+    4 : ""
+};
